6fb184ca64afb8cb371e7779defb866b5d2c7809
moustaxx <maciasm1303@gmail.com>
2025-01-09 14:03:32 +0100

    Make doctor's calendar functional

--- src/routes/DoctorSchedule/DoctorSchedule.tsx
+++ src/routes/DoctorSchedule/DoctorSchedule.tsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
-import { format, parse, startOfWeek, getDay } from "date-fns";
+import { format, parse, startOfWeek, getDay, addMinutes } from "date-fns";
 import { pl } from "date-fns/locale";
 
 import styles from "./DoctorSchedule.module.css";
 import "./react-big-calendar.css";
+import { useSupabase } from "../../contexts/SupabaseProvider";
+import { IDoctor } from "../../types/IDoctor";
+import { IVisit } from "../../types/IVisit";
+import { IPatient } from "../../types/IPatient";
 
 const messages = {
     next: "Następny",
@@ -17,23 +21,68 @@ const messages = {
     noEventsInRange: "Brak wydarzeń w tym zakresie",
 };
 
-; const locales = { "pl": pl };
+const locales = { "pl": pl };
+
+interface IEventItem {
+    title: string;
+    start: Date;
+    end: Date;
+}
 
 const localizer = dateFnsLocalizer({ format, parse, startOfWeek, getDay, locales });
 
 const DoctorSchedule: React.FC = () => {
-    const [events, setEvents] = useState([
-        {
-            start: new Date(2024, 10, 21, 10, 0), // 21 listopada 2024, godzina 10:00
-            end: new Date(2024, 10, 21, 11, 0),
-            title: "Spotkanie z zespołem",
-        },
-        {
-            start: new Date(2024, 10, 22, 14, 0), // 22 listopada 2024, godzina 14:00
-            end: new Date(2024, 10, 22, 15, 0),
-            title: "Prezentacja projektu",
-        },
-    ]);
+    const supabase = useSupabase();
+    const [loading, setLoading] = useState(false);
+
+    const [events, setEvents] = useState<IEventItem[]>([]);
+
+    useEffect(() => {
+        const handleSearch = async () => {
+            setLoading(true);
+            try {
+                const session = await supabase.auth.getSession();
+                const doctorRes = await supabase.from("doctor")
+                    .select("id, email")
+                    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                    .eq("email", session.data.session!.user.email)
+                    .single();
+
+                const doctor = doctorRes.data as Partial<IDoctor>;
+
+                const visitsRes = await supabase
+                    .from("visit")
+                    .select("patient_id, doctor_id, date, confirmed, patient(first_name, last_name)")
+                    .eq("doctor_id", doctor.id);
+
+                if (visitsRes.error) {
+                    console.error("Blad podczas wyszukiwania:", visitsRes.error.message);
+                } else {
+                    const visits = visitsRes.data as (IVisit & { patient: Partial<IPatient> })[];
+
+                    setEvents(visits.map((visit) => {
+                        const startDate = new Date(visit.date);
+                        const endDate = addMinutes(startDate, 30);
+                        return {
+                            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+                            title: `${visit.patient.first_name} ${visit.patient.last_name}`,
+                            start: startDate,
+                            end: endDate,
+                        };
+                    }));
+                }
+            } catch (err) {
+                console.error("Nieoczekiwany blad:", err);
+            } finally {
+                setLoading(false);
+            }
+        };
+        void handleSearch();
+    }, [supabase]);
+
+    if (loading) {
+        return <div className={styles.root}>Ładowanie...</div>;
+    }
 
     return (
         <div className={styles.root}>
