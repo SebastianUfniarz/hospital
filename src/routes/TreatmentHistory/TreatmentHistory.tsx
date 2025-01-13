9b7c533669442f453e2e5033a3fda09b3f54afce
moustaxx <maciasm1303@gmail.com>
2025-01-13 20:10:42 +0100

    Make TreatmentHistory functionable

--- src/routes/TreatmentHistory/TreatmentHistory.tsx
+++ src/routes/TreatmentHistory/TreatmentHistory.tsx
@@ -2,53 +2,44 @@ import { useEffect, useState } from "react";
 import { useSupabase } from "../../contexts/SupabaseProvider";
 import styles from "./TreatmentHistory.module.css";
 import { IPatientRecord } from "../../types/IPatientRecord";
+import { IPatient } from "../../types/IPatient";
+import { IDoctor } from "../../types/IDoctor";
+
+interface IPatientRecordData extends IPatientRecord {
+    patient: Pick<IPatient, "id">;
+    doctor: Pick<IDoctor, "first_name" | "last_name" | "specialization">;
+}
 
 const TreatmentHistory: React.FC = () => {
     const supabase = useSupabase();
-    const [history, setHistory] = useState<IPatientRecord[]>([]);
+    const [history, setHistory] = useState<IPatientRecordData[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
     useEffect(() => {
         const fetchTreatmentHistory = async () => {
             try {
-                const { data: patientRecords, error: patientRecordsError } = await supabase
+                const session = await supabase.auth.getSession();
+                const patientRes = await supabase.from("patient")
+                    .select("id, email")
+                    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                    .eq("email", session.data.session!.user.email)
+                    .single();
+
+                const patientRecordsRes = await supabase
                     .from("patient_record")
-                    .select("id, diagnosis, diagnosis_date, patient_id, doctor_id")
-                    .eq("doctor_id", 21);
+                    .select(`
+                        id, diagnosis, recommendations, performed_treatments, diagnosis_date, patient_id, doctor_id,
+                        patient(id),
+                        doctor(first_name, last_name, specialization)`)
+                    .eq("patient.id", patientRes.data?.id)
+                    .order("diagnosis_date");
 
-                if (patientRecordsError) throw patientRecordsError;
+                const patientRecords = patientRecordsRes.data as unknown as IPatientRecordData[];
 
-                const recordsWithDetails = await Promise.all(
-                    patientRecords.map(async (record) => {
-                        interface INames {
-                            first_name: string;
-                            last_name: string;
-                        }
+                if (patientRecordsRes.error) throw patientRecordsRes.error;
 
-                        const res1 = await supabase
-                            .from("patient")
-                            .select("first_name, last_name")
-                            .eq("id", record.patient_id)
-                            .single();
-                        const patientData = res1.data as INames;
-
-                        const res2 = await supabase
-                            .from("doctor")
-                            .select("first_name, last_name")
-                            .eq("id", record.doctor_id)
-                            .single();
-                        const doctorData = res2.data as INames;
-
-                        return {
-                            ...record,
-                            patient_name: `${patientData.first_name} ${patientData.last_name}`,
-                            doctor_name: `${doctorData.first_name} ${doctorData.last_name}`,
-                        };
-                    }),
-                );
-
-                setHistory(recordsWithDetails);
+                setHistory(patientRecords);
             } catch (error) {
                 console.error(error);
                 setErrorMessage("Nie udało się pobrać historii leczenia");
@@ -62,38 +53,37 @@ const TreatmentHistory: React.FC = () => {
     return (
         <div className={styles.root}>
             <h3>Historia Leczenia</h3>
-            {loading
-                ? (
-                        <p>Ładowanie...</p>
-                    )
-                : errorMessage
-                    ? (
-                            <p style={{ color: "red" }}>{errorMessage}</p>
-                        )
-                    : (
-                            <table className={styles.historyTable}>
-                                <thead>
-                                    <tr>
-                                        <th>Pacjent</th>
-                                        <th>Doktor</th>
-                                        <th>Diagnoza</th>
-                                        <th>Data diagnozy</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {history.map(record => (
-                                        <tr key={record.id}>
-                                            <td>{record.patient_name}</td>
-                                            <td>{record.doctor_name}</td>
-                                            <td>{record.diagnosis}</td>
-                                            <td>{record.diagnosis_date}</td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        )}
+            {loading && <div>Ładowanie...</div>}
+            {errorMessage && <div>{errorMessage}</div>}
+            {!loading && !errorMessage && history.length === 0 && <div>Brak danych</div>}
+            {!loading && !errorMessage && history.length !== 0 && (
+                <table className={styles.historyTable}>
+                    <thead>
+                        <tr>
+                            <th>Data</th>
+                            <th>Lekarz</th>
+                            <th>Specializacja</th>
+                            <th>Diagnoza</th>
+                            <th>Wykonane zabiegi</th>
+                            <th>Zalecenia</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {history.map(record => (
+                            <tr key={record.id}>
+                                <td>{record.diagnosis_date}</td>
+                                <td>{record.doctor.first_name} {record.doctor.last_name}</td>
+                                <td>{record.doctor.specialization}</td>
+                                <td>{record.diagnosis}</td>
+                                <td>{record.performed_treatments ?? "-"}</td>
+                                <td>{record.recommendations ?? "-"}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+
         </div>
     );
 };
-
 export default TreatmentHistory;
--- src/types/IPatientRecord.ts
+++ src/types/IPatientRecord.ts
@@ -1,8 +1,8 @@
 export interface IPatientRecord {
-    patient_name: string;
-    doctor_name: string;
     id: number;
     diagnosis: string;
+    recommendations?: string;
+    performed_treatments?: string;
     diagnosis_date: string;
     patient_id: number;
     doctor_id: number;
